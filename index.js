918cd45f64a5622e5d5e6a30d253c904d19220eb
Misskhat <misskhat.hossen@gmail.com>
2025-11-25 01:22:12 +0600

    code recheck for deploy

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 
-//TODO:confidential after create .env file it will be change
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bt6oyo.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
